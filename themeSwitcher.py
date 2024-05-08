92489374cf3c0015342dd84cf633668bfbf15ed9
POTAP322 <dragonkingmore@gmail.com>
2024-05-09 00:13:10 +0300

    2

--- themeSwitcher.py
+++
@@ -1,8 +0,0 @@
-class themeSwitcher:
-    def __init__(self):
-        self.themes = {'light':'css/light', 'dark':'css/dark'}
-        self.curTheme = 'light'
-
-    def changeTheme(self,theme):
-        if theme in self.curTheme:
-            self.curTheme = theme
