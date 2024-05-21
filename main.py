3bf494bdb1297db9e0440cab3bf71a92940e8d16
Potap <dragonkingmore@gmail.com>
2024-05-21 22:57:42 +0300

    готово :)

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
 import os
 import shutil
 
-from PIL import Image, ImageFilter
+import psutil
+from PIL import Image, ImageFilter, ImageEnhance, ImageOps
 from flask import Flask, render_template, request, send_file
+
+
+
 
 current_dir = os.getcwd()
 
 app = Flask(__name__)
+def clear_image_folders():
 
-
+    for folder in [orig_img_folder, mod_img_folder]:
+        for filename in os.listdir(folder):
+            file_path = os.path.join(folder, filename)
+            if os.path.isfile(file_path):
+                os.remove(file_path)
 # название страницы
 @app.route("/index")
 @app.route("/")
 def index():
     # очищаем папки с картинками
-    for filename in os.listdir(orig_img_folder):
-        file_path = os.path.join(orig_img_folder, filename)
-        if os.path.isfile(file_path):
-            os.remove(file_path)
-    for filename in os.listdir(mod_img_folder):
-        file_path = os.path.join(mod_img_folder, filename)
-        if os.path.isfile(file_path):
-            os.remove(file_path)
-
+    clear_image_folders()
     return render_template("index.html")
 
 
@@ -38,26 +39,104 @@ def upload_image():
         if len(os.listdir(orig_img_folder)) <= 1:
             uploaded_file.save(orig_img_folder + 'orig_image.jpg')
             shutil.copy(orig_img_folder + 'orig_image.jpg', os.path.join(mod_img_folder, 'mod_image.jpg'))
+            uploaded_file.close()
+
+
+
 
     return render_template('index.html')
+@app.route('/process_reset', methods=['GET', 'POST'])
+def reset():
+    shutil.copy(orig_img_folder + 'orig_image.jpg', os.path.join(mod_img_folder, 'mod_image.jpg'))
+    return 'Success'
 
 
 @app.route('/process_blur_checkbox', methods=['POST'])
 def process_blur_checkbox():
     data = request.get_json()
     print(data)
-
-    if 'my_checkbox' in data and data['my_checkbox'] is True:
-        mod_image_path = os.path.join(mod_img_folder, 'mod_image.jpg')
-        mod_image = Image.open(mod_image_path)
+    mod_image_path = os.path.join(mod_img_folder, 'mod_image.jpg')
+    mod_image = Image.open(mod_image_path)
+    if 'blured_checkbox' in data and data['blured_checkbox'] is True:
         blurred_image = mod_image.filter(ImageFilter.BLUR)
 
         blurred_image.convert('RGB').save(mod_image_path)
     else:
         print("Галочка не нажата")
 
-    # отправляем ответ
+    # отправляем ответ в js
     return send_file(mod_image_path, mimetype='image/jpeg')
+
+@app.route('/process_negative_checkbox', methods=['POST'])
+def process_negative_checkbox():
+    data = request.get_json()
+    mod_image_path = os.path.join(mod_img_folder, 'mod_image.jpg')
+    mod_image = Image.open(mod_image_path)
+
+    # Преобразование изображения в режим RGB
+    mod_image = mod_image.convert('RGB')
+
+    if 'negative_checkbox' in data and data['negative_checkbox'] is True:
+        negative_image = ImageOps.invert(mod_image)
+        negative_image.save(mod_image_path)
+    else:
+        print("Галочка не нажата")
+
+    # Отправляем ответ в js
+    return send_file(mod_image_path, mimetype='image/jpeg')
+@app.route('/process_sharpness_slider', methods=['POST'])
+def process_sharpness_slider():
+    data = request.get_json()
+    # print(data)
+
+    mod_image_path = os.path.join(mod_img_folder, 'mod_image.jpg')
+    mod_image = Image.open(mod_image_path)
+    mod_image_tmp = mod_image
+
+    sharpness = int(data['sharpnessValue']) / 100.0
+    sharped_image = ImageEnhance.Sharpness(mod_image_tmp).enhance(sharpness)
+
+    mod_image.close()
+    sharped_image.convert('RGB').save(mod_image_path)
+
+
+    return send_file(mod_image_path, mimetype='image/jpeg')
+
+
+@app.route('/process_contrast_slider', methods=['POST'])
+def process_contrast_slider():
+    data = request.get_json()
+
+    mod_image_path = os.path.join(mod_img_folder, 'mod_image.jpg')
+    mod_image = Image.open(mod_image_path)
+    contrast = int(data['contrastValue']) / 100.0
+
+    contrast_image = ImageEnhance.Contrast(mod_image).enhance(1 + contrast/4)
+
+    mod_image.close()
+    contrast_image.convert('RGB').save(mod_image_path)
+
+
+    return send_file(mod_image_path, mimetype='image/jpeg')
+
+
+@app.route('/process_brightness_slider', methods=['POST'])
+def process_brightness_slider():
+    data = request.get_json()
+
+    mod_image_path = os.path.join(mod_img_folder, 'mod_image.jpg')
+    mod_image = Image.open(mod_image_path)
+    brightness = int(data['brightnessValue']) / 100.0
+
+    max_brightness_change = 0.5  # Максимальное изменение яркости, которое вы хотите применить
+
+    contrast_image = ImageEnhance.Brightness(mod_image).enhance(1 + brightness * max_brightness_change)
+
+    mod_image.close()
+    contrast_image.convert('RGB').save(mod_image_path)
+
+    return send_file(mod_image_path, mimetype='image/jpeg')
+
 
 
 @app.route("/info")
--- static/js/script.js
+++ static/js/script.js
@@ -11,6 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const selectedFile = event.target.files[0];
         img.src = URL.createObjectURL(selectedFile);
         uploadFile(selectedFile);
+        resetTools()
     });
 
     function uploadFile(file) {
@@ -26,11 +27,36 @@ document.addEventListener('DOMContentLoaded', () => {
             console.error('Ошибка при отправке файла на сервер:', error);
         });
     }
+    function resetTools() {
+        const blur_checkbox = document.getElementById('checkbox1');
+        const sharpnessSlider = document.getElementById('sharpnessSlider');
+        const contrastSlider = document.getElementById('contrastSlider');
+        sharpnessSlider.value = 0;
+        contrastSlider.value = 0;
+        brightnessSlider.value = 0;
+        checkbox1.checked = false;
+        checkbox1.disabled = false;
+        checkbox2.checked = false;
+        checkbox2.disabled = false;
+
+    }
+
+const resetBtn = document.getElementById('reset-btn');
+resetBtn.addEventListener('click', async (event) => {
+    const response = await fetch('/process_reset', { method: 'POST' });
+    if (response.ok) {
+        // Обработка успешного ответа
+        img.src = 'static/image/modPicture/mod_image.jpg?' + Math.random();
+        resetTools()
+    } else {
+        console.error('Ошибка при выполнении запроса');
+    }
+});
 
 
 
     const blur_checkbox = document.getElementById('checkbox1');
-    const myForm = document.getElementById('myForm');
+    const myForm = document.getElementById('blurForm');
 
     blur_checkbox.addEventListener('click', () => {
     if (checkbox1.checked) {
@@ -38,7 +64,7 @@ document.addEventListener('DOMContentLoaded', () => {
         // Отправка данных на сервер
         fetch('/process_blur_checkbox', {
             method: 'POST',
-            body: JSON.stringify({ my_checkbox: blur_checkbox.checked }),
+            body: JSON.stringify({ blured_checkbox: blur_checkbox.checked }),
             headers: {
                 'Content-Type': 'application/json'
             }
@@ -55,5 +81,96 @@ document.addEventListener('DOMContentLoaded', () => {
         img.src = 'static/image/origPicture/orig_image.jpg?' + Math.random();
     }
 });
+
+
+    const negative_checkbox = document.getElementById('checkbox2');
+    const myForm2 = document.getElementById('negativeForm');
+
+    negative_checkbox.addEventListener('click', () => {
+    if (checkbox2.checked) {
+        console.log('Чекбокс нажат');
+        // Отправка данных на сервер
+        fetch('/process_negative_checkbox', {
+            method: 'POST',
+            body: JSON.stringify({ negative_checkbox: negative_checkbox.checked }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(response => {
+            //обновление изображения на веб-странице
+
+            //math что бы не кешировал браузер старую версию картинки
+            img.src = 'static/image/modPicture/mod_image.jpg?' + Math.random();
+        }).catch(error => {
+            console.error('Ошибка при отправке данных на сервер:', error);
+        });
+    } else {
+        console.log('Чекбокс не нажат');
+        img.src = 'static/image/origPicture/orig_image.jpg?' + Math.random();
+    }
+});
+
+
+
+    const sharpnessSlider = document.getElementById('sharpnessSlider');
+    const sharpnessValue = document.getElementById('sharpnessValue');
+    sharpnessSlider.addEventListener('input', () => {
+    sharpnessValue.value = sharpnessSlider.value;
+    //console.log(sharpnessValue.value);
+            fetch('/process_sharpness_slider', {
+            method: 'POST',
+            body: JSON.stringify({ sharpnessValue: sharpnessValue.value }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(response => {
+            //обновление изображения на веб-странице
+            img.src = 'static/image/modPicture/mod_image.jpg?' + Math.random();
+        }).catch(error => {
+            console.error('Ошибка при отправке данных на сервер:', error);
+        });
+
+});
+
+    const contrastSlider = document.getElementById('contrastSlider');
+    const contrastValue = document.getElementById('contrastValue');
+    contrastSlider.addEventListener('input', () => {
+    contrastValue.value = contrastSlider.value;
+            fetch('/process_contrast_slider', {
+            method: 'POST',
+            body: JSON.stringify({ contrastValue: contrastValue.value }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(response => {
+            //обновление изображения на веб-странице
+            img.src = 'static/image/modPicture/mod_image.jpg?' + Math.random();
+        }).catch(error => {
+            console.error('Ошибка при отправке данных на сервер:', error);
+        });
+
+});
+    const brightnessSlider = document.getElementById('brightnessSlider');
+    const brightnessValue = document.getElementById('brightnessValue');
+    brightnessSlider.addEventListener('input', () => {
+        brightnessValue.value = brightnessSlider.value;
+            fetch('/process_brightness_slider', {
+            method: 'POST',
+            body: JSON.stringify({ brightnessValue: brightnessValue.value }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(response => {
+            //обновление изображения на веб-странице
+            img.src = 'static/image/modPicture/mod_image.jpg?' + Math.random();
+        }).catch(error => {
+            console.error('Ошибка при отправке данных на сервер:', error);
+        });
+
+});
+
+
+
+
 
 });
